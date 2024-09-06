a4192e540836ccd3dc0b16634d98004ca752b30f
Peter <github@loginp.me>
2024-09-06 15:52:10 -0500

    added startup code and some input validation

--- battleship.py
+++ battleship.py
@@ -10,8 +10,10 @@ Aug 31 2024
 
 '''
 TO ADD:
-- Input validity checks
+- 
 '''
+
+import os
 
 # ANSI Coloring for text
 RED = '\033[91m'
@@ -23,8 +25,13 @@ DEFAULT = '\033[0m'
 # Print colored text
 # print(f'{GREEN}Hello world!{DEFAULT}')
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 p1_game_board = [[' ']*10]*10
 p2_game_board = [[' ']*10]*10
+x = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9}
+y = [str(num) for num in range(1,11)]
 
 def print_board(game_board):
     print(BLUE, end='')
@@ -67,10 +74,36 @@ def game_setup():
 
     # Player 2 game board setup
 
+def run_game():
+    while True:
+        clear_screen()
+        print_board(p1_game_board)
+
+        # Player 1 Attacks
+        while True:
+            attack_pos = input(f'{RED}Player 1{DEFAULT}: Which cell would you like to attack?):\n')
+            if check_move(attack_pos, p2_game_board):
+                break
+        
+        # confirm whether attack was valid and make changes
+
+        # Player 2 Attacks
+    
+def check_move(attack_pos, game_board): # returns True if valid move
+    if attack_pos[0].lower() in x.keys() and attack_pos[1:] in y:
+        # check for already attacked positions
+        if p2_game_board[x[attack_pos[0].lower()]][int(attack_pos[1:])-1] != ' ':
+            print(f'Cell has already been attacked!')
+            return False
+        else:
+            return True
+    else:
+        print(f'Please enter a valid cell to attack! [A1]')
+        return False
 
 def main():
     game_setup()
-    # print_board(p1_game_board)
+    run_game()
 
 if __name__ == '__main__':
     main()
